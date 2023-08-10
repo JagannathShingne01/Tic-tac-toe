ba2ef738db9463e1c304d602e979da841ee1b16f
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-08-10 20:06:59 +0530

    structure done

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Card from "../card/Card";
+import "./Grid.css"
+function Grid ({ numberOfCards }){
+ const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+
+ return(
+    <div className="gird">
+        {board.map((el, idx)=> <Card key = {idx} />)}
+    </div>    
+ );
+}
+export default Grid;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import Icon from "../icon/icon";
+import "./Card.css"
+function Card({ player }){
+    let icon = <Icon />
+    if(player == "X"){
+        icon = <Icon name="cross" />
+    } else if(player == "O"){
+        icon = <Icon name="circle" />
+    }
+    return(
+        <div className="card">
+            {icon}
+        </div>
+    )
+}
+export default Card;
